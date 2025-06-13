99982ddee659c4ec77090fee5bb234c5ff59935d
William Morrison <110057755+WSMorrison@users.noreply.github.com>
2025-06-13 11:23:16 -0500

    Add fourth stop

--- Rail Control/rail_control/rail_control.ino
+++ Rail Control/rail_control/rail_control.ino
@@ -1,83 +1,115 @@
-#define A 8 // Digital pins for motor driver direction.
-#define B 9
-#define PWM 7 // Analog pinout for motor driver speed.
-#define Sensor1 2 // Sensor.
-#define Sensor2 0
+//#define pwrLight LED
+#define motorDriverNBA 27 // Digital pins for motor driver direction.
+#define motorDriverNBB 26
+#define PWM 28 // Analog pinout for motor driver speed.
+#define startNBA 0 // Northbound Start A Sensor
+#define endNBA 6 // Northbound End A Sensor
+#define stop1NBA 2 // Northbound Stop 1 A Sensor
+#define stop2NBA 4 // Northbound Stop 2 A Sensor
 
 int moving;
 int direction = 1;
-int scheduledStop;
+int nBEndStop;
+int nBMidStop;
 // User definable variables
-int maxSpeed = 100;
+int maxSpeed = 75;
 int embarkTime = 5500;
 
 void setup() {
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(Sensor1, INPUT);
-  pinMode(Sensor2, INPUT);
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
+  pinMode(motorDriverNBA, OUTPUT);
+  pinMode(motorDriverNBB, OUTPUT);
+  pinMode(startNBA, INPUT);
+  pinMode(endNBA, INPUT);
+  pinMode(stop1NBA, INPUT);
+  pinMode(stop2NBA, INPUT);
+  digitalWrite(motorDriverNBA, HIGH);
+  digitalWrite(motorDriverNBB, LOW);
   analogWrite(PWM, 010);
 
-  pinMode(25, OUTPUT);//Added for Pico, sice it has no onboard POWER LED.
-  digitalWrite(25, HIGH);//Using the LED on pin GP25 as a power indicator.
+  //pinMode(pwrLight, OUTPUT);//Added for Pico, sice it has no onboard POWER LED.
+  //digitalWrite(pwrLight, HIGH);//Using the LED on pin GP25 as a power indicator.
 
   delay(200);
 }
 
 void trainStart(){
   analogWrite(PWM, (0.50 * maxSpeed));
-  delay(2000);
-  analogWrite(PWM, (0.75 * maxSpeed));
+  delay(2500);
+  //analogWrite(PWM, (0.75 * maxSpeed));
 }
 
 void trainMax(){
   analogWrite(PWM, maxSpeed);
   moving = 1;
-  scheduledStop = 0;
+  nBEndStop = 0;
+  nBMidStop = 0;
 }
 
 void trainStop(){
-  analogWrite(PWM, 75);
+  analogWrite(PWM, (0.50 * maxSpeed));
   delay(1000);
   analogWrite(PWM, 010);
 }
 
 void changeDirection(){
   if (direction == 1){
-    digitalWrite(A, LOW);
-    digitalWrite(B, HIGH);
+    digitalWrite(motorDriverNBA, LOW);
+    digitalWrite(motorDriverNBB, HIGH);
     direction = 0;
   } else if (direction == 0) {
-    digitalWrite(A, HIGH);
-    digitalWrite(B, LOW);
+    digitalWrite(motorDriverNBA, HIGH);
+    digitalWrite(motorDriverNBB, LOW);
     direction = 1;
   }
 }
 
 void loop() {
 
-  int readSensor1 = digitalRead(Sensor1);
-  int readSensor2 = digitalRead(Sensor2);
+// Reading inputs as part of loop
+  int readstartNBA = digitalRead(startNBA);
+  int readendNBA = digitalRead(endNBA);
+  int readstop1NBA = digitalRead(stop1NBA);
+  int readstop2NBA = digitalRead(stop2NBA);
 
-  if ((readSensor1 == LOW or readSensor2 == LOW) && moving == 1){ // LOW when detecting 
+// End Stop
+  if (((readstartNBA == LOW or readendNBA == LOW) && (readstop1NBA == HIGH && readstop2NBA == HIGH)) && moving == 1){ // LOW when detecting 
     moving = 0;
     trainStop();
     delay(2000);
     changeDirection();
-    scheduledStop = millis();
+    nBEndStop = millis();
   }
 
-  if ((readSensor1 == LOW or readSensor2 == LOW) && moving == 0){
-    if (millis() - scheduledStop >= embarkTime){
+  if (((readstartNBA == LOW or readendNBA == LOW) && (readstop1NBA == HIGH && readstop2NBA == HIGH)) && moving == 0){
+    if (millis() - nBEndStop >= embarkTime){
       trainStart();
     }
   }
 
-  if ((readSensor1 == HIGH && readSensor2 == HIGH) && moving == 0){ // HIGH when not detecting
-    if (millis() - scheduledStop >= (embarkTime + 5500)){
+  if (((readstartNBA == HIGH && readendNBA == HIGH) && (readstop1NBA == HIGH && readstop2NBA == HIGH)) && moving == 0){ // HIGH when not detecting
+    if (millis() - nBEndStop >= (embarkTime + 5500)){
       trainMax();
     }
   }
+
+// Mid Stop
+  if ((readstartNBA == HIGH && readendNBA == HIGH) && (readstop1NBA == LOW or readstop2NBA == LOW) && moving == 1){
+    moving = 0;
+    trainStop();
+    delay(2000);
+    nBMidStop = millis();  
+  }
+
+  if ((readstartNBA == HIGH && readendNBA == HIGH) && (readstop1NBA == LOW or readstop2NBA == LOW) && moving == 0){
+    if (millis() - nBMidStop >= embarkTime){
+      trainStart();
+    }
+  }
+
+  if ((readstartNBA == HIGH && readendNBA == HIGH) && (readstop1NBA == HIGH && readstop2NBA == HIGH) && moving == 0){
+    if (millis() - nBMidStop >= (embarkTime + 5500)){
+      trainMax();
+    }
+  }
+
 }
