2ddf9113dcd7793257252089f59e29598ab9c8d2
William Morrison <110057755+WSMorrison@users.noreply.github.com>
2025-06-13 11:15:35 -0500

    Add second stop

--- Rail Control/rail_control/rail_control.ino
+++ Rail Control/rail_control/rail_control.ino
@@ -1,19 +1,21 @@
 #define A 8 // Digital pins for motor driver direction.
 #define B 9
 #define PWM 7 // Analog pinout for motor driver speed.
-#define Sensor 2 // Sensor.
+#define Sensor1 2 // Sensor.
+#define Sensor2 0
 
 int moving;
 int direction = 1;
 int scheduledStop;
 // User definable variables
-int maxSpeed = 150;
-int embarkTime = 7500;
+int maxSpeed = 100;
+int embarkTime = 5500;
 
 void setup() {
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
-  pinMode(Sensor, INPUT);
+  pinMode(Sensor1, INPUT);
+  pinMode(Sensor2, INPUT);
   digitalWrite(A, HIGH);
   digitalWrite(B, LOW);
   analogWrite(PWM, 010);
@@ -25,9 +27,9 @@ void setup() {
 }
 
 void trainStart(){
-  analogWrite(PWM, (0.33 * maxSpeed));
+  analogWrite(PWM, (0.50 * maxSpeed));
   delay(2000);
-  analogWrite(PWM, (0.66 * maxSpeed));
+  analogWrite(PWM, (0.75 * maxSpeed));
 }
 
 void trainMax(){
@@ -56,23 +58,25 @@ void changeDirection(){
 
 void loop() {
 
-  int readSensor = digitalRead(Sensor);
+  int readSensor1 = digitalRead(Sensor1);
+  int readSensor2 = digitalRead(Sensor2);
 
-  if (readSensor == LOW && moving == 1){ // LOW when detecting 
+  if ((readSensor1 == LOW or readSensor2 == LOW) && moving == 1){ // LOW when detecting 
     moving = 0;
     trainStop();
+    delay(2000);
     changeDirection();
     scheduledStop = millis();
   }
 
-  if (readSensor == LOW && moving == 0){
+  if ((readSensor1 == LOW or readSensor2 == LOW) && moving == 0){
     if (millis() - scheduledStop >= embarkTime){
       trainStart();
     }
   }
 
-  if (readSensor == HIGH && moving == 0){ // HIGH when not detecting
-    if (millis() - scheduledStop >= (embarkTime + 3500)){
+  if ((readSensor1 == HIGH && readSensor2 == HIGH) && moving == 0){ // HIGH when not detecting
+    if (millis() - scheduledStop >= (embarkTime + 5500)){
       trainMax();
     }
   }
