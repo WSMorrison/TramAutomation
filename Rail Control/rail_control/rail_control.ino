3389b5b60e403cb4c4f2d5258cabb03d4c4ade46
William Morrison <110057755+WSMorrison@users.noreply.github.com>
2025-07-27 15:27:50 -0500

    Fix global variables for consistency. Add definitions and reads for multiple stops, commented out.

--- Rail Control/rail_control/rail_control.ino
+++ Rail Control/rail_control/rail_control.ino
@@ -1,28 +1,45 @@
-#define motorDriverNBA 27 // Digital pins for Nothbound motor driver direction.
-#define motorDriverNBB 26
+// NORTHBOUND LINE
+#define nbMotorDriverA 27 // Digital pins for Nothbound motor driver direction.
+#define nbMotorDriverB 26
 #define nbPWM 28 // Analog pinout for Northbound motor driver speed.
 
-#define startNBA 0 // Northbound Start A Sensor.
-#define endNBA 6 // Northbound End A Sensor.
-#define stop1NBA 2 // Northbound Stop 1 A Sensor.
+#define nbSouthTerminal 0 // Northbound start position sensor.
+#define nbNorthTerminal 7 // Northbound end postion sensor.
+#define nbStop1 1 // Northbound middle stop sensors.
+#define nbStop2 2
+#define nbStop3 3
+#define nbStop4 4
+#define nbStop5 5
+#define nbStop6 6
+// NORTHBOUND LINE
 
-#define motorDriverSBA 21 // Digital pins for Southbound motor driver direction.
-#define motorDriverSBB 20
+// SOUTHBOUND LINE
+#define sbMotorDriverA 21 // Digital pins for Southbound motor driver direction.
+#define sbMotorDriverB 20
 #define sbPWM 22 // Analog pinout for Southbound motor driver speed.
 
-#define startSBA 8 // Southbound Start A Sensor.
-#define endSBA 14// Southbound End A Sensor.
-#define stop1SBA 10 // Southbound Stop 1 A Sensor.
+#define sbNorthTerminal 8 // Southbound start position sensor.
+#define sbSouthTerminal 15// Southbound end position sensor.
+#define sbStop1 9 // Southbound middle stop sensors.
+#define sbStop2 10
+#define sbStop3 11
+#define sbStop4 12
+#define sbStop5 13
+#define sbStop6 14
+// SOUTHBOUND LINE
 
-#define loopIndicatorLED 15 // LED that indicates when loop cycles.
+#define emergencyStopSensor 18 // Emergency Stop sensor circuit, multiple sensors in parallel.
+#define emergencyIndicatorLED 19 // Emergency Stop indicator LED, currently used to indicate loop operation.
 
-int nbMoving; // Northbound global variables
+// Northbound global variables
+int nbMoving; 
 int nbDirection;
 int nbTrainSpeed;
 int nbTrainGo;
 int nbTrainStop;
 
-int sbMoving; // Southbound global variables
+// Southbound global variables
+int sbMoving; 
 int sbDirection;
 int sbTrainSpeed;
 int sbTrainGo;
@@ -30,49 +47,49 @@ int sbTrainStop;
 
 void nbChangeDirection(){ // Change direction of Northbound train and direction value.
   if (nbDirection == 0){
-    digitalWrite(motorDriverNBA, LOW);
-    digitalWrite(motorDriverNBB, HIGH);
+    digitalWrite(nbMotorDriverA, LOW);
+    digitalWrite(nbMotorDriverB, HIGH);
     nbDirection = 1;
   } else if (nbDirection == 1) {
-    digitalWrite(motorDriverNBA, HIGH);
-    digitalWrite(motorDriverNBB, LOW);
+    digitalWrite(nbMotorDriverA, HIGH);
+    digitalWrite(nbMotorDriverB, LOW);
     nbDirection = 0;
   }
 }
 
 void sbChangeDirection(){ // Change direction of Southbound train and direction value.
   if (sbDirection == 0){
-    digitalWrite(motorDriverSBA, LOW);
-    digitalWrite(motorDriverSBB, HIGH);
+    digitalWrite(sbMotorDriverA, LOW);
+    digitalWrite(sbMotorDriverB, HIGH);
     sbDirection = 1;
   } else if (sbDirection == 1) {
-    digitalWrite(motorDriverSBA, HIGH);
-    digitalWrite(motorDriverSBB, LOW);
+    digitalWrite(sbMotorDriverA, HIGH);
+    digitalWrite(sbMotorDriverB, LOW);
     sbDirection = 0;
   }
 }
 
 void setup() {
 
-  pinMode(motorDriverNBA, OUTPUT); // Set Northbound rPi pinouts.
-  pinMode(motorDriverNBB, OUTPUT);
-  pinMode(startNBA, INPUT);
-  pinMode(endNBA, INPUT);
-  pinMode(stop1NBA, INPUT);
-  digitalWrite(motorDriverNBA, HIGH); // Set Northbound train direction and "speed" to start.
-  digitalWrite(motorDriverNBB, LOW);
+  pinMode(nbMotorDriverA, OUTPUT); // Set Northbound rPi pinouts.
+  pinMode(nbMotorDriverB, OUTPUT);
+  pinMode(nbSouthTerminal, INPUT);
+  pinMode(nbNorthTerminal, INPUT);
+  pinMode(nbStop1, INPUT);
+  digitalWrite(nbMotorDriverA, HIGH); // Set Northbound train direction and "speed" to start.
+  digitalWrite(nbMotorDriverB, LOW);
   analogWrite(nbPWM, nbTrainSpeed); 
 
-  pinMode(motorDriverSBA, OUTPUT); // Set Southbound rPi pinouts.
-  pinMode(motorDriverSBB, OUTPUT);
-  pinMode(startSBA, INPUT);
-  pinMode(endSBA, INPUT);
-  pinMode(stop1SBA, INPUT);
-  digitalWrite(motorDriverSBA, HIGH); // Set Southbound train direction and "speed" to start.
-  digitalWrite(motorDriverSBB, LOW);
+  pinMode(sbMotorDriverA, OUTPUT); // Set Southbound rPi pinouts.
+  pinMode(sbMotorDriverB, OUTPUT);
+  pinMode(sbNorthTerminal, INPUT);
+  pinMode(sbSouthTerminal, INPUT);
+  pinMode(sbStop1, INPUT);
+  digitalWrite(sbMotorDriverA, HIGH); // Set Southbound train direction and "speed" to start.
+  digitalWrite(sbMotorDriverB, LOW);
   analogWrite(sbPWM, sbTrainSpeed); 
 
-  pinMode(loopIndicatorLED, OUTPUT);
+  pinMode(emergencyIndicatorLED, OUTPUT);
 
   nbMoving = 0; // Set start values for Northbound variables.
   nbDirection = 0;
@@ -90,26 +107,34 @@ void setup() {
 
 void loop() {
 
-  digitalWrite(loopIndicatorLED, HIGH); // Turn on loop indicator LED for testing frequency of loop.
+  digitalWrite(emergencyIndicatorLED, LOW); // Turns out it's just pretty much constantly on, so no delay I guess?
 
-  int readStartNBA = digitalRead(startNBA); // Read sensor values each loop.
-  int readEndNBA = digitalRead(endNBA);
-  int readStop1NBA = digitalRead(stop1NBA);
-  int readStartSBA = digitalRead(startSBA);
-  int readEndSBA = digitalRead(endSBA);
-  int readStop1SBA = digitalRead(stop1SBA);
-
-  digitalWrite(loopIndicatorLED, LOW); // Turns out it's just pretty much constantly on, so no delay I guess?
+  int readNbSouthTerminal = digitalRead(nbSouthTerminal); // Read sensor values each loop.
+  int readNbNorthTerminal = digitalRead(nbNorthTerminal);
+  int readNbStop1 = digitalRead(nbStop1);
+  //int readNbStop2 = digitalRead(nbStop2);
+  //int readNbStop3 = digitalRead(nbStop3);
+  //int readNbStop4 = digitalRead(nbStop4);
+  //int readNbStop5 = digitalRead(nbStop5);
+  //int readNbStop6 = digitalRead(nbStop6);
+  int readSbNorthTerminal = digitalRead(sbNorthTerminal);
+  int readSbSouthTerminal = digitalRead(sbSouthTerminal);
+  int readSbStop1 = digitalRead(sbStop1);
+  //int readSbStop2 = digitalRead(sbStop2);
+  //int readSbStop3 = digitalRead(sbStop3);
+  //int readSbStop4 = digitalRead(sbStop4);
+  //int readSbStop5 = digitalRead(sbStop5);
+  //int readSbStop6 = digitalRead(sbStop6);
   
   if ((nbMoving == 1) && millis() >= nbTrainGo) { // Handle Northbound moving train at terminals, check time to ignore repeat inputs.
-    if ((readStartNBA == LOW) or (readEndNBA == LOW)) {
+    if ((readNbSouthTerminal == LOW) or (readNbNorthTerminal == LOW)) {
       nbMoving = 0;
       nbTrainSpeed = 10;
       analogWrite(nbPWM, nbTrainSpeed);
       nbChangeDirection();
       nbTrainStop = (millis() + 4000);
     }
-    else if (readStop1NBA == LOW && millis() >= nbTrainGo) { // Handle Northbound moving train at midstops, check time to ignore repeat inputs.
+    else if (readNbStop1 == LOW && millis() >= nbTrainGo) { // Handle Northbound moving train at midstops, check time to ignore repeat inputs.
       nbMoving = 0;
       nbTrainSpeed = 10;
       analogWrite(nbPWM, nbTrainSpeed);
@@ -117,29 +142,34 @@ void loop() {
     }
   } else if ((nbMoving == 0) && (millis() >= nbTrainStop)) { // Handle Northbound stopped train, check time to allow passengers to board.
     nbMoving = 1;
-    nbTrainGo = (millis() + 3000);
+    nbTrainGo = (millis() + 2000);
     nbTrainSpeed = 75;
     analogWrite(nbPWM, nbTrainSpeed);
   }
 
   if ((sbMoving == 1) && millis() >= sbTrainGo) { // Handle Southbound moving train at terminals, check time to ignore repeat inputs.
-    if ((readStartSBA == LOW) or (readEndSBA == LOW)) {
+    if ((readSbNorthTerminal == LOW) or (readSbSouthTerminal == LOW)) {
       sbMoving = 0;
       sbTrainSpeed = 10;
       analogWrite(sbPWM, sbTrainSpeed);
       sbChangeDirection();
       sbTrainStop = (millis() + 4000);
     }
-    else if (readStop1SBA == LOW && millis() >= sbTrainGo) { // Handle Southbound moving train at midstops, check time to ignore repeat inputs.
+    else if (readSbStop1 == LOW && millis() >= sbTrainGo) { // Handle Southbound moving train at midstops, check time to ignore repeat inputs.
       sbMoving = 0;
-      sbTrainSpeed = 010;
+      sbTrainSpeed = 10;
       analogWrite(sbPWM, sbTrainSpeed);
       sbTrainStop = (millis() + 4000);
     }
   } else if ((sbMoving == 0) && (millis() >= sbTrainStop)) { // Handle Southbound stopped train, check time to allow passengers to board.
     sbMoving = 1;
-    sbTrainGo = (millis() + 3000);
+    sbTrainGo = (millis() + 2000);
     sbTrainSpeed = 85;
     analogWrite(sbPWM, sbTrainSpeed);
   }
+
+  digitalWrite(emergencyIndicatorLED, HIGH); // Turn on loop indicator LED for testing frequency of loop.
+  delay(1);
+  digitalWrite(emergencyIndicatorLED, LOW); // Turns out it's just pretty much constantly on, so no delay I guess?
+
 }
